79a054d06c8ae46c8af2f6a48198fe7f5ef4ff59
Sebastian Jimenez <sebasg1325@hotmail.com>
2021-08-31 22:24:15 -0500

    Servidor

---
+++ servidor/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+require('dotenv').config();
+//require('./configuraciones/databaseMongodb');
+
+//Configuracion.
+app.set('port', process.env.PORT);
+app.set('views', path.join(__dirname, 'views'));
+
+//Middlewares.
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Routes.
+
+
+//Iniciar el servidor.
+app.listen(app.get('port'), () => {
+    console.log(`Start on port ${app.get('port')}`);
+});

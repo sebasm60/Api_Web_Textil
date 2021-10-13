dec2d66f0392ddf44f86d4272846433c82cd3c7e
Sebastian Jimenez <sebasg1325@hotmail.com>
2021-10-13 01:51:28 -0500

    a

--- cliente/public/js/main.js
+++ cliente/public/js/main.js
@@ -1,12 +1,11 @@
 window.onload = function() {
 
     //Ajustes del navar
-    const showNavbar = (toggleId, navId, bodyId, headerId, divId) => {
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
         const toggle = document.getElementById(toggleId);
         const nav = document.getElementById(navId);
         const bodypd = document.getElementById(bodyId);
         const headerpd = document.getElementById(headerId);
-        const div = document.getElementById(divId);
 
         if (toggle && nav && bodypd && headerpd) {
             toggle.addEventListener('click', () => {
@@ -14,12 +13,11 @@ window.onload = function() {
                 toggle.classList.toggle('bx-x');
                 bodypd.classList.toggle('body-pd');
                 headerpd.classList.toggle('body-pd');
-                div.classList.toggle('div-productos');
             });
         };
     };
 
-    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header', 'productos-container');
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
     const linkColor = document.querySelectorAll('.nav__link');
 
     function colorLink() {
